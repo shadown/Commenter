520b3b255a453d24f51301e4cb285a64e73e2ce1
shadown <shadown@gmail.com>
2012-07-10 14:35:07 +0200

    Initial commit.

---
+++ Commenter.py
@@ -0,0 +1,229 @@
+#
+# author: Sergio 'shadown' Alvarez
+#
+
+import sublime, sublime_plugin
+
+import os					# path and file location
+import cPickle as pickle	# metadata storage
+
+############## 
+"""SETTINGS"""
+##############
+settings = sublime.load_settings('Commenter.sublime-settings').get # (key, None)
+
+if settings('debug'):
+	print "debug:"				, settings("debug")
+	print "show_context_menus:"	, settings("show_context_menus")
+	print "bookmarks_folder:"	, settings("bookmarks_folder")
+	print "bookmarks_prefix:"	, settings("bookmarks_prefix")
+	print "bookmarks_postfix:"	, settings("bookmarks_postfix")
+	print "bookmarks_ext:"		, settings("bookmarks_ext")
+	print "os_sep", os.sep
+
+def getDatabaseForRootFolder(rootFolder):
+	_folder  = settings("bookmarks_folder")
+	_prefix  = settings("bookmarks_prefix")
+	_postfix = settings("bookmarks_postfix")
+	_ext     = settings("bookmarks_ext")
+	_sep     = os.sep
+
+	# if the destination dir doesn't exist create it.
+	if not os.path.isdir(_folder):
+		os.mkdir(_folder)
+
+	return _folder + _sep + _prefix + os.path.basename(rootFolder) + _postfix + '.' + _ext
+
+
+# directory where all bookmarks will be stored.
+bookmarks_folder = None
+
+##################
+"""Add Comments"""
+##################
+class CommenterAddCommentCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		# create self._bookmark
+		self._craftBookmark()
+
+		# we don't want to create a database for a single file.
+		if not self._bookmark['rootFolder']:
+			print "[!] This plug-in is for SourceCode Trees, not for individual files!"
+			return
+
+		_prev_comment = self._getPreviousComment()
+
+		window        = self.view.window()
+		window.show_input_panel("Enter Bookmark Comment:", _prev_comment, self._addComment, None, None)
+
+	def _addComment(self, comment):
+		# if the comment is empty remove the bookmark is it exists
+		if not comment:
+			self._delComment()
+			return
+
+		# load pre-existing db if any
+		if os.path.isfile(self._dbFile):
+			# load previous comments
+			comments = pickle.load(open(self._dbFile, "rb"))
+		else:
+			comments = dict()
+
+		filePath = self._bookmark['filePath']
+		fileName = self._bookmark['fileName']
+		fileLine = self._bookmark['fileLine']
+		
+		if not comments.has_key(filePath):
+			comments[filePath] = dict()
+
+		if not comments[filePath].has_key(fileName):
+			comments[filePath][fileName] = dict()
+
+		# if there is a pre-existing comment at that line, we overwrite it
+		comments[filePath][fileName][fileLine] = comment
+
+		# save to our comments db
+		pickle.dump(comments, open(self._dbFile, "wb" ))
+
+	def _delComment(self):
+		# load pre-existing db if any
+		if os.path.isfile(self._dbFile):
+			# load previous comments
+			comments = pickle.load(open(self._dbFile, "rb"))
+		else:
+			# nothing to delete
+			return
+
+		filePath = self._bookmark['filePath']
+		fileName = self._bookmark['fileName']
+		fileLine = self._bookmark['fileLine']
+		
+		if not comments.has_key(filePath):
+			# filePath key doesn't exist, no key to delete
+			return
+
+		if not comments[filePath].has_key(fileName):
+			# fileName key doesn't exist, no key to delete
+			return
+
+		if not comments[filePath][fileName].has_key(fileLine):
+			# fileLine key doesn't exist, no key to delete
+			return
+		else:
+			comments[filePath][fileName].pop(fileLine)
+
+		if not len(comments[filePath][fileName].keys()):
+			# there are no more keys in that fileName, remove it
+			comments[filePath].pop(fileName)
+
+		if not len(comments[filePath].keys()):
+			# there are no more keys in that filePath, remove it
+			comments.pop(filePath)
+
+		# save to our comments db
+		pickle.dump(comments, open(self._dbFile, "wb" ))
+
+	def _craftBookmark(self):
+		self._bookmark = dict()
+
+		# file location
+		_filePath, _fileName = os.path.split(self.view.file_name())
+
+		# cursor location -> line, column, we only care about the line number
+		_sel   = self.view.sel()
+		_point = _sel[0].begin()
+		_line  =  self.view.rowcol(_point)[0]
+		_line  += 1 # rowcol() starts at 0, but 'goto_line' starts at 1
+
+		# get active folders
+		_folders = self.view.window().folders()
+
+		# find which folder (aka: project) we are working on.
+		_actualFolder = None
+		for f in _folders:
+			if f in _filePath:
+				_actualFolder = f
+				break
+
+		# build bookmark
+		self._bookmark['rootFolder'] = _actualFolder
+		self._bookmark['filePath']   = _filePath
+		self._bookmark['fileName']   = _fileName
+		self._bookmark['fileLine']   = _line
+
+	def _getPreviousComment(self):
+		# create path to DB
+		self._dbFile = getDatabaseForRootFolder(self._bookmark['rootFolder'])
+
+		if os.path.isfile(self._dbFile):
+			# load previous comments
+			comments = pickle.load(open(self._dbFile, "rb"))
+
+			filePath = self._bookmark['filePath']
+			fileName = self._bookmark['fileName']
+			fileLine = self._bookmark['fileLine']
+			
+			if comments.has_key(filePath):
+				if comments[filePath].has_key(fileName):
+					if comments[filePath][fileName].has_key(fileLine):
+						return comments[filePath][fileName][fileLine]
+
+		return ''
+
+class CommenterBrowseCommentsCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		# file location
+		_filePath, _fileName = os.path.split(self.view.file_name())
+
+		# get active folders
+		_folders = self.view.window().folders()
+
+		# find which folder (aka: project) we are working on.
+		_actualFolder = None
+		for f in _folders:
+			if f in _filePath:
+				_actualFolder = f
+				break
+
+		# if not working on a folder return, since it's a single file
+		if not _actualFolder: return
+
+		# get path to DB
+		self._dbFile = getDatabaseForRootFolder(_actualFolder)
+
+		if os.path.isfile(self._dbFile):
+			# load previous comments
+			comments = pickle.load(open(self._dbFile, "rb"))
+		else:
+			return
+
+		if settings('debug'):
+			print "Actual Comments:", comments
+
+		if not len(comments): return
+
+		self._comments_list = list()
+		self._comments_line = list()
+		for path in comments.keys():
+			for fname in comments[path].keys():
+				for line in comments[path][fname].keys():
+					cmnt = comments[path][fname][line]
+					self._comments_list.append("%s, line: %lu\t %s" % (fname, line, cmnt))
+
+					filePath = path+os.sep+fname
+					self._comments_line.append((line, filePath))
+
+		window = self.view.window()
+
+		window.show_quick_panel(self._comments_list, self._gotoBookmark)
+
+	def _gotoBookmark(self, index):
+		# if no bookmark was selected, return
+		if index == -1:
+			return
+
+		window = self.view.window()
+
+		view = window.open_file(os.path.normpath(self._comments_line[index][1]))
+
+		view.run_command("goto_line", {"line": self._comments_line[index][0]})
---
+++ README.md
@@ -0,0 +1,64 @@
+# Description
+
+Commenter is a [Sublime Text 2](http://www.sublimetext.com/) package that allows you to create bookmarks with comments, which is useful with reviewing/auditing source code.
+
+It creates handles bookmarks per working directory you are working on. - It is NOT meant to be used with single files -
+
+## The workflow goes like
+
+* You open a folder in ST2, which contains the source code you are about to audit
+* Open files with in that folder and press (Cmd + Ctrl + m)
+
+A separete bookmark database will be created for each "Folder" you have open via "File"->"Open...", when you create your first bookmark, in the directory specified in Commenter.sublime-settings
+
+# Install
+
+## Manual Install
+
+Go to your Packages subdirectory under ST2's data directory:
+
+* Windows: %APPDATA%\Sublime Text 2
+* OS X: ~/Library/Application Support/Sublime Text 2
+* Linux: ~/.config/sublime-text-2
+* Portable Installation: Sublime Text 2/Data
+
+And clone the repo:
+
+git clone git://github.com/shadown/Commenter.git
+
+## Configuration
+
+Open Commenter.sublime-settings and customize the setting as you please.
+
+"""
+{
+    "debug"             : false,
+    "bookmarks_folder"  : "/Users/shadown/.subl_commenter",
+    "bookmarks_prefix"  : "",
+    "bookmarks_postfix" : "_comments",
+    "bookmarks_ext"     : "db"
+}
+"""
+
+Be sure to set "bookmarks_folder" to the directory you want to store the bookmark-database files.
+
+### The settings are self-explanatories, but anyway here it goes:
+
+"debug": set it to "true" if you want to see some debug messages in the console embedded in ST2.
+"bookmarks_folder": complete path to the folder where you want to create all the bookmarks-databases.
+"bookmarks_prefix": prefix you want to add to the bookmark-database filename.
+"bookmarks_postfix": postfix you want to add to the bookmark-database filename.
+"bookmarks_ext": extension you want your database to use.
+
+# Shortcuts
+
+## Create a bookmark 
+	(Cmd + Ctrl + m) and enter your comment.
+## Access a bookmark 
+	(Cmd + Ctrl + b) select the bookmart you want to jump to.
+## Modify a bookmark's comment
+	(Cmd + Ctrl + m) in the line of the bookmark and modify it.
+## Remore a bookmark
+	(Cmd + Ctrl + m) empty the bookmark's comment, press (enter), and the bookmark will be removed as well.
+
+That's all!
